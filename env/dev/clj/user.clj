c5beb9e41ccfea0e020980241f683ff0978abb3b
nacorv <nick@piss.tech>
2020-09-30 18:35:32 -0400

    initial commit

---
+++ env/dev/clj/user.clj
@@ -0,0 +1,64 @@
+(ns user
+  "Userspace functions you can run by default in your local REPL."
+  (:require
+   [my-app.config :refer [env]]
+    [clojure.pprint]
+    [clojure.spec.alpha :as s]
+    [expound.alpha :as expound]
+    [mount.core :as mount]
+    [my-app.figwheel :refer [start-fw stop-fw cljs]]
+    [my-app.core :refer [start-app]]
+    [my-app.db.core]
+    [conman.core :as conman]
+    [luminus-migrations.core :as migrations]))
+
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
+
+(add-tap (bound-fn* clojure.pprint/pprint))
+
+(defn start
+  "Starts application.
+  You'll usually want to run this on startup."
+  []
+  (mount/start-without #'my-app.core/repl-server))
+
+(defn stop
+  "Stops application."
+  []
+  (mount/stop-except #'my-app.core/repl-server))
+
+(defn restart
+  "Restarts application."
+  []
+  (stop)
+  (start))
+
+(defn restart-db
+  "Restarts database."
+  []
+  (mount/stop #'my-app.db.core/*db*)
+  (mount/start #'my-app.db.core/*db*)
+  (binding [*ns* 'my-app.db.core]
+    (conman/bind-connection my-app.db.core/*db* "sql/queries.sql")))
+
+(defn reset-db
+  "Resets database."
+  []
+  (migrations/migrate ["reset"] (select-keys env [:database-url])))
+
+(defn migrate
+  "Migrates database up for all outstanding migrations."
+  []
+  (migrations/migrate ["migrate"] (select-keys env [:database-url])))
+
+(defn rollback
+  "Rollback latest database migration."
+  []
+  (migrations/migrate ["rollback"] (select-keys env [:database-url])))
+
+(defn create-migration
+  "Create a new up and down migration file with a generated timestamp and `name`."
+  [name]
+  (migrations/create name (select-keys env [:database-url])))
+
+
